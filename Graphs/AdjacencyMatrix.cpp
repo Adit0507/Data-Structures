b07ec792764904065f2a34a353b657956b5fa284
Adit0507 <adi05072002@gmail.com>
2022-08-21 21:38:02 +0530

    Minor fix

--- Graphs/AdjacencyMatrix.cpp
+++ Graphs/AdjacencyMatrix.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main(){
     int n, m;
     cin >> n >> m;
-    // m ->No. of Nodes
-    // n -> No. of Edges
+    // n ->No. of Nodes
+    // m -> No. of Edges
     int adj[n +1][n +1];
     for(int i = 0; i < m; i++){
         int u, v;
